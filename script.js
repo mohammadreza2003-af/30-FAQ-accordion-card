d96bd790f45c7f3d45d0eb3f286e523fa8ec85dc
mohammad2003mrz <gorbanben10@gmail.com>
2023-09-06 18:03:34 +0430

    update animation

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
 
 const headingSecondary = document.querySelectorAll(".heading-secondary");
 const subDis = document.querySelectorAll(".sub-dis");
+const arrow = document.querySelectorAll(".arrow");
 
 headingSecondary.forEach((el) => {
   el.addEventListener("click", function () {
     el.nextElementSibling.classList.toggle("ac-hide");
+    el.nextElementSibling.classList.toggle("sub-dis-ac");
+    el.closest(".item").children[2].classList.toggle("arrow-ac");
   });
 });
